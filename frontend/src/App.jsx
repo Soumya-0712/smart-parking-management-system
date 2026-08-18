0e885728fc360fe1fd51a8533dd10081d6426480
Soumya-0712 <soumya.paul0712@gmail.com>
2026-08-18 17:27:08 +0530

    feat(frontend): initialize React application

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-amber-400 flex justify-center">Tailwind Setup Done!</h1>
+    </>
+  );
+}
+
+export default App;
